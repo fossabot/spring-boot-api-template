b7c089315414e2b0355974d9f85c2b6b63473537
dangdinhtai0001 <dangdinhtai0001@gmail.com>
2021-01-08 00:25:52 +0700

    .

--- MODULE-COMMON/src/main/java/com/phoenix/common/exception/runtime/UnavailableImplementationException.java
+++ MODULE-COMMON/src/main/java/com/phoenix/common/exception/runtime/UnavailableImplementationException.java
@@ -31,7 +31,9 @@ package com.phoenix.common.exception.runtime;
  */
 public final class UnavailableImplementationException extends RuntimeException {
 
-    private static final String DEFAULT_NOT_FOUND_MESSAGE = "Unable to find an implementation for %s using java.util.ServiceLoader. Ensure you include a backing implementation .jar in the classpath, for example jjwt-impl.jar, or your own .jar for custom implementations.";
+    private static final String DEFAULT_NOT_FOUND_MESSAGE = "Unable to find an implementation for %s using java.util.ServiceLoader. " +
+            "Ensure you include a backing implementation .jar in the classpath, for example jjwt-impl.jar, or your own .jar " +
+            "for custom implementations.";
 
     public UnavailableImplementationException(final Class klass) {
         super(String.format(DEFAULT_NOT_FOUND_MESSAGE, klass));
